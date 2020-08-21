f15f456167bd762d67f0dd307d9a6c7e790fed0d
appub <appub@hotmail.com>
2020-08-21 15:43:14 +0530

    added support for randomising contents in html list

--- htmlmocker.js
+++ htmlmocker.js
@@ -1,27 +1,97 @@
 console.log("script loaded");
 
-let classList = ["hm-text", "hm-img"];
+let classList = ["hm-text","hm-img","hm-list"];
 
-function generateRandomString(length) {
-  var result = "";
-  var characters =
+function generateRandomString(constraints) {
+  // Generate random string of given length from a
+  // set of predefined charachters
+  let min = Math.ceil(Number(constraints["size"].split("__")[0]));
+  let max = Math.floor(Number(constraints["size"].split("__")[1]));
+
+  // random number between range Max and Min inclusive
+  let length = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+  let result = "";
+  let characters =
     "ABCD\nE FGHIJKL MNOPQRST UVWXY\n\tZabcdefg hijklmnopq rstuvwxyz0\t123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
+  let charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
+
   return result;
 }
 
+function getRandomImage(constraints) {
+  // use lorem picsum api to get random images respecting given constarints
+  let minWidth = Math.ceil(
+    Number(constraints["size"].split("__")[0].split("x")[0])
+  );
+  let minHeight = Math.ceil(
+    Number(constraints["size"].split("__")[0].split("x")[1])
+  );
+  let maxWidth = Math.ceil(
+    Number(constraints["size"].split("__")[1].split("x")[0])
+  );
+  let maxHeight = Math.ceil(
+    Number(constraints["size"].split("__")[1].split("x")[1])
+  );
+  let randomWidth =
+    Math.floor(Math.random() * (maxWidth + 1 - minWidth)) + minWidth;
+  let randomHeight =
+    Math.floor(Math.random() * (maxHeight + 1 - minHeight)) +
+    minHeight;
+  return "https://picsum.photos/" + randomWidth + "/" + randomHeight;
+}
+
+function getConstraints(className, kind) {
+  return className
+    .split(" ")
+    .filter((x) => x.startsWith("hm-") && x != kind)
+    .reduce((acc, c) => {
+      acc[c.split("-")[1]] = c.split("-")[2];
+      return acc;
+    }, {});
+}
+
+function generateRandomList(length){
+  let list = document.querySelector(".hm-list");
+  let listClone = list.cloneNode(true);
+  list.innerText = "";
+  let className =  listClone.children[0].children[0].className;
+  let childType = className.split(" ")[0];
+  if (childType == "hm-list-text"){
+    constraints = getConstraints(className, "hm-list-text");
+  }
+  else if(childType == "hm-list-img"){
+    constraints = getConstraints(className, "hm-list-img");
+  }
+
+  // generae new list of given length and random childs
+  for (let i = 0; i < length; i++) {
+    if (childType == "hm-list-text"){
+      let child = listClone.children[0].children[0].cloneNode(true);
+      child.innerText = generateRandomString(constraints);
+      let li = document.createElement("li");
+      li.appendChild(child);
+      list.appendChild(li);
+    }
+    else if(childType == "hm-list-img"){
+      let child = listClone.children[0].children[0].cloneNode(true);
+      child.src = getRandomImage(constraints);
+      let li = document.createElement("li");
+      li.appendChild(child);
+      list.appendChild(li);
+    }
+  }
+}
+
+
+
 classList.forEach(function (kind) {
   document.querySelectorAll("." + kind).forEach(function (el) {
-    let constraints = el.className
-      .split(" ")
-      .filter((x) => x.startsWith("hm-") && x != kind)
-      .reduce((acc, c) => {
-        acc[c.split("-")[1]] = c.split("-")[2];
-        return acc;
-      }, {});
+    let constraints = getConstraints(el.className, kind);
 
     setInterval(function () {
       // Generate a random text / image source / list that respects the constraints but explores the
@@ -29,31 +99,28 @@ classList.forEach(function (kind) {
       switch (kind) {
         case "hm-text":
           // Generate random text
-          // TODO: Respect the specified constraints.size etc
+          el.innerText = generateRandomString(constraints);
+          break;
+
+        case "hm-img":
+          // replace image with random images respecting the constraints
+          el.src = getRandomImage(constraints);
+          break;
+
+        case "hm-list":
+          // get size constraints of the list
           let min = Math.ceil(Number(constraints["size"].split("__")[0]));
           let max = Math.floor(Number(constraints["size"].split("__")[1]));
-
-          // let samples = ["", "small text", "text \n\n with whitespace chars\t\ngoes here", "username with 😀 in it"];
-          // random number between range Max and Min inclusive
+          // random size respecting the given constraints
           let length = Math.floor(Math.random() * (max + 1 - min)) + min;
-          console.log(length);
-          el.innerText = generateRandomString(length);
-
-          break;
-        case "hm-img":
-          let min_width = Math.ceil(Number(constraints["size"].split("__")[0].split("x")[0]));
-          let min_height = Math.ceil(Number(constraints["size"].split("__")[0].split("x")[1]));
-          let max_width = Math.ceil(Number(constraints["size"].split("__")[1].split("x")[0]));
-          let max_height = Math.ceil(Number(constraints["size"].split("__")[1].split("x")[1]));
-          let random_width = Math.floor(Math.random() * (max_width + 1 - min_width)) + min_width;
-          let random_height = Math.floor(Math.random() * (max_height + 1 - min_height)) + min_height;
-          el.src = "https://picsum.photos/" + random_width + "/" + random_height
-
+          
+          //generate random list
+          generateRandomList(length);
           break;
 
         default:
           console.log("Unknown HTML Mocker kind: " + kind);
       }
-    }, 10000);
+    }, 6000);
   });
 });
